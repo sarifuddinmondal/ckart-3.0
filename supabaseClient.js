5a95caed48171bff7966c0e5e1f8408e168ff0cf
sarifuddinmondal <sarifuddinm9@gmail.com>
2026-02-07 00:40:22 +0530

    Refactor supabaseClient.js for clarity

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// আপনার প্রজেক্টের সঠিক URL এবং আপনার দেওয়া Anon Public Key এখানে সেট করা হয়েছে
+// আপনার সুপাবেস প্রজেক্টের সঠিক ডিটেইলস
 const supabaseUrl = 'https://wuuspreptqyqzvudtdvq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1dXNwcmVwdHF5cXp2dWR0ZHZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzODMwNzksImV4cCI6MjA4NTk1OTA3OX0.kKyGaRAmTMDOrFoB3XSqmMv-xtocFNlh5S8HkVe95qA'
 
